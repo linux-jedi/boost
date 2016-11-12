fd7f0003f9136688ad93c2bdf595e2d9cdeb7a6e
linux-jedi <cbthomas1997@gmail.com>
2016-11-12 12:16:14 -0800

    hopefully final wsgi tweaks

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,16 @@
-activate_this = '/var/www/prod/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
-
+import os
 import sys
-sys.path.insert(0, '/var/www/html/boost')
+import site
 
+# Add virtualenv site packages
+site.addsitedir(os.path.join(os.path.dirname(__file__), 'env/local/lib64/python2.7/site-packages'))
+
+# Path of execution
+sys.path.append('/var/www/boost)
+
+# Fired up virtualenv before include application
+activate_env = os.path.expanduser(os.path.join(os.path.dirname(__file__), 'env/bin/activate_this.py'))
+execfile(activate_env, dict(__file__=activate_env))
+
+# import boost as application
 from boost import app as application
