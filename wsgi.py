ee559b34b155c44f7236b52581de2a6d605a1fd2
linux-jedi <cbthomas1997@gmail.com>
2016-11-12 12:22:22 -0800

    more...

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ import site
 site.addsitedir(os.path.join(os.path.dirname(__file__), 'env/local/lib64/python2.7/site-packages'))
 
 # Path of execution
-sys.path.append('/var/www/boost)
+sys.path.append('/var/www/html/boost')
 
 # Fired up virtualenv before include application
 activate_env = os.path.expanduser(os.path.join(os.path.dirname(__file__), 'env/bin/activate_this.py'))
